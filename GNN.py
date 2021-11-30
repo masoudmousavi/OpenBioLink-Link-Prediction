91f5c1e0fc86cd1e41adab3a7f862cae588793af
masoudmousavi <72153807+masoudmousavi@users.noreply.github.com>
2021-11-30 16:15:42 +0330

    added basis decomposition regularization to GNN

--- GNN.py
+++ GNN.py
@@ -12,12 +12,12 @@ class GNN(Module):
     self.relation_embedder = Embedding(self.num_relations, fully_connected_for_tail_dims[-1])
     if x_feature == 'one-hot':
       self.entity_embedder = Embedding(embedding_dims[0], embedding_dims[1])
-      first_conv_layer = [RGCNConv(embedding_dims[1], conv_dims[0], self.num_relations)]
+      first_conv_layer = [RGCNConv(embedding_dims[1], conv_dims[0], self.num_relations, num_bases=Global.NUM_BASES.value)]
     elif x_feature == 'identity':
-      first_conv_layer = [RGCNConv(1, conv_dims[0], self.num_relations)]
+      first_conv_layer = [RGCNConv(1, conv_dims[0], self.num_relations, num_bases=Global.NUM_BASES.value)]
     conv_list = first_conv_layer + \
                                 [
-                                  RGCNConv(conv_dims[i], conv_dims[i+1], self.num_relations)
+                                  RGCNConv(conv_dims[i], conv_dims[i+1], self.num_relations, num_bases=Global.NUM_BASES.value)
                                   for i in range(len(conv_dims[:-1]))
                                 ]
   
@@ -65,12 +65,12 @@ class GNN(Module):
     ####################################### One-Hot Entity Encoder #######################################
     if self.x_feature == 'one-hot':
       x = self.entity_embedder(x).reshape(self.entity_embedder.weight.shape[0], -1)
-      
     ############################################## Identity Encoder ################################################
     elif self.x_feature == 'identity':
-      x = torch.ones(x.shape[0], 1)
-      if self.training:
-        x = self.emb_dropout(x)
+      x = torch.ones(x.shape[0], 1).to(Global.DEVICE.value)
+      
+    if self.training:
+      x = self.emb_dropout(x)
     ####################################### RGCN Encoder #######################################
     for conv in self.conv_layers[:-1]:
       x = conv(x, edge_index=edge_index, edge_type=edge_type)
@@ -84,15 +84,17 @@ class GNN(Module):
     ####################################### Decoder #######################################
     positive_heads = x[data.edge_index_supervision[0]]
     positive_tails = x[data.edge_index_supervision[1]]
-    positive_relations = self.relation_embedder(data.edge_type_supervision) # relation encoder
+    positive_relations = data.edge_type_supervision
 
     negative_heads = x[data.edge_index_negative[0]]
     negative_tails = x[data.edge_index_negative[1]]
-    negative_relations = self.relation_embedder(data.edge_type_negative)
+    negative_relations = data.edge_type_negative
 
     heads = torch.cat((positive_heads, negative_heads))
     relations = torch.cat((positive_relations, negative_relations))
+    relations = self.relation_embedder(relations)
     tails = torch.cat((positive_tails, negative_tails))
+
     perm_index = torch.randperm(heads.shape[0])
     heads = heads[perm_index]
     tails = tails[perm_index]
